cd1a8180096bc207ff2edacba9cc1afcfe843579
lcvoelker <lcvoelkerr@gmail.com>
2024-01-31 13:28:28 -0500

    commit to revert

--- src/main/java/plc/project/Lexer.java
+++ src/main/java/plc/project/Lexer.java
@@ -86,6 +86,7 @@ public final class Lexer {
         if (peek("[0]", "[1-9]")) { //leading zeros
             throw new ParseException("Leading 0", chars.index);
         }
+
         while (peek("[0-9]"))
             match("[0-9]");
         if (peek("[.]", "[0-9]")) { //decimal, number
