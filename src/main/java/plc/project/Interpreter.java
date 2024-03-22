361ade13caf748cc3b1cbe36b2088b942d5a0d3e
lcvoelker <lcvoelkerr@gmail.com>
2024-03-22 09:57:59 -0400

    1 todo

--- src/main/java/plc/project/Interpreter.java
+++ src/main/java/plc/project/Interpreter.java
@@ -26,11 +26,15 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
     @Override
     public Environment.PlcObject visit(Ast.Source ast) {
        // throw new UnsupportedOperationException(); //TODO
-        for(Ast.Field f : ast.getFields()) {
-            visit(f);
+         List<Ast.Global> global = ast.getGlobals();
+        for (int i = 0; i < global.size(); i++) {
+            Ast.Global global = global.get(i);
+            visit(global);
         }
-        for(Ast.Method m : ast.getMethods()) {
-            visit(m);
+        List<Ast.Functions> function = ast.getFunctions();
+        for (int i = 0; i < function.size(); i++) {
+            Ast.Function function = function.get(i);
+            visit(function);
         }
         try {
             return scope.lookupFunction("main", 0).invoke(Collections.emptyList());
