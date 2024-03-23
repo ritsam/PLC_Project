c9544c186b84b894d5cdcb894fd2b686e06e6414
lcvoelker <lcvoelkerr@gmail.com>
2024-03-23 16:14:52 -0400

    switch + funct + binary

--- src/main/java/plc/project/Interpreter.java
+++ src/main/java/plc/project/Interpreter.java
@@ -36,11 +36,8 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
             Ast.Function f = function.get(i);
             visit(f);
         }
-        try {
-            return scope.lookupFunction("main", 0).invoke(Collections.emptyList());
-        } catch (Scope.LookupException e) {
-            throw new RuntimeException("Main function not found in source.");
-        }
+        return scope.lookupFunction("main", 0).invoke(Collections.emptyList());
+        //throw new RuntimeException("Main function not found in source.");        
     }
 
     @Override
@@ -56,7 +53,28 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
 
     @Override
     public Environment.PlcObject visit(Ast.Function ast) {
-        throw new UnsupportedOperationException(); //TODO
+        //throw new UnsupportedOperationException(); //TODO 
+        scope.defineFunction(ast.getName(), ast.getParameters().size(), args -> {
+        Scope currentScope = scope; 
+        //new child scope
+        scope = new Scope(currentScope); 
+        try {
+            for (int i = 0; i < ast.getParameters().size(); i++) {
+                scope.defineVariable(ast.getParameters().get(i), true, args.get(i));
+            }
+            for (Ast.Statement statement : ast.getStatements()) {
+                visit(statement);
+            }
+        } catch (Return returnValue) {
+            return returnValue.value; 
+        } finally {
+            scope = currentScope; 
+        }
+        return Environment.NIL; 
+    });
+    return Environment.NIL; 
+}
+
     }
 
     @Override
@@ -100,12 +118,14 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
         while (requireType(Boolean.class, visit(ast.getCondition())) != null) {
             try {
                 scope = new Scope(scope);
-                if (!((Boolean) visit(ast.getCondition()).getValue())) { //false-else
+                //false-else
+                if (!((Boolean) visit(ast.getCondition()).getValue())) { 
                     for (Ast.Statement e : ast.getElseStatements()) {
                         visit(e);
                     }
                 } else {
-                    for (Ast.Statement t : ast.getThenStatements()) { //then
+                    //then
+                    for (Ast.Statement t : ast.getThenStatements()) { 
                         visit(t);
                     }
                 }
@@ -118,9 +138,39 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
 
     @Override
     public Environment.PlcObject visit(Ast.Statement.Switch ast) {
-        throw new UnsupportedOperationException();//TODO
-
+        //throw new UnsupportedOperationException();//TODO
+        Environment.PlcObject conditionValue = visit(ast.getCondition()); 
+        Scope currentScope = scope; 
+        scope = new Scope(currentScope); 
+        try {
+            boolean matchedCaseExecuted = false; 
+            for (Ast.Statement.Case caseStmt : ast.getCases()) {
+                // matched case
+                if (caseStmt.getValue().isPresent() && Objects.equals(visit(caseStmt.getValue().get()), conditionValue)) {            
+                    for (Ast.Statement statement : caseStmt.getStatements()) {
+                        visit(statement);
+                    }
+                    matchedCaseExecuted = true;
+                    break; 
+                }
+            }
+            // no case matched + has default case then execte stmts
+            if (!matchedCaseExecuted) {
+                for (Ast.Statement.Case caseStmt : ast.getCases()) {
+                    if (!caseStmt.getValue().isPresent()) { 
+                        for (Ast.Statement statement : caseStmt.getStatements()) {
+                            visit(statement);
+                        }
+                        break; 
+                    }
+                }
+            }
+        } finally {
+            scope = currentScope; 
+        }
+        return Environment.NIL; 
     }
+
 
     @Override
     public Environment.PlcObject visit(Ast.Statement.Case ast) {
@@ -169,7 +219,55 @@ public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
 
     @Override
     public Environment.PlcObject visit(Ast.Expression.Binary ast) {
-        throw new UnsupportedOperationException(); //TODO
+        //throw new UnsupportedOperationException(); //TODO
+        switch (ast.getOperator()) {
+            case "&&":
+                return Environment.create(requireType(Boolean.class, visit(ast.getLeft())) && requireType(Boolean.class, visit(ast.getRight())));
+            case "||":
+                return Environment.create(requireType(Boolean.class, visit(ast.getLeft())) || requireType(Boolean.class, visit(ast.getRight())));
+            case "<":
+            case ">":
+                Comparable leftComparable = requireType(Comparable.class, visit(ast.getLeft()).getValue());
+                Comparable rightComparable = requireType(Comparable.class, visit(ast.getRight()).getValue());
+                int comparison = leftComparable.compareTo(rightComparable);
+                if (ast.getOperator().equals("<")) {
+                    return Environment.create(comparison < 0);
+                } 
+                else {
+                    return Environment.create(comparison > 0);
+                }
+            case "==":
+                return Environment.create(Objects.equals(visit(ast.getLeft()), visit(ast.getRight())));
+            case "!=":
+                return Environment.create(!Objects.equals(visit(ast.getLeft()), visit(ast.getRight())));
+            case "+":
+                Environment.PlcObject left = visit(ast.getLeft());
+                Environment.PlcObject right = visit(ast.getRight());
+                if (left.getValue() instanceof String || right.getValue() instanceof String) {
+                    return Environment.create(left.getValue().toString() + right.getValue().toString());
+                } 
+                else if (left.getValue() instanceof BigInteger && right.getValue() instanceof BigInteger) {
+                    return Environment.create(((BigInteger) left.getValue()).add((BigInteger) right.getValue()));
+                } 
+                else if (left.getValue() instanceof BigDecimal && right.getValue() instanceof BigDecimal) {
+                    return Environment.create(((BigDecimal) left.getValue()).add((BigDecimal) right.getValue()));
+                } 
+                else {
+                    throw new RuntimeException("Invalid operands for + operator.");
+                }
+            case "-":
+                return Environment.create(requireType(BigInteger.class, visit(ast.getLeft())).subtract(requireType(BigInteger.class, visit(ast.getRight()))));
+            case "*":
+                return Environment.create(requireType(BigInteger.class, visit(ast.getLeft())).multiply(requireType(BigInteger.class, visit(ast.getRight()))));
+            case "/":
+                BigInteger rightDiv = requireType(BigInteger.class, visit(ast.getRight()));
+                if (rightDiv.equals(BigInteger.ZERO)) throw new RuntimeException("Division by zero.");
+                    return Environment.create(requireType(BigInteger.class, visit(ast.getLeft())).divide(rightDiv));
+            case "^":
+                return Environment.create(requireType(BigInteger.class, visit(ast.getLeft())).pow(requireType(BigInteger.class, visit(ast.getRight())).intValueExact()));
+            default:
+                throw new RuntimeException("Unsupported operator: " + ast.getOperator());
+        }
     }
 
     @Override
